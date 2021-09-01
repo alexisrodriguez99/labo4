7d2617a4459a51f5ad59543d1f301ffa58b28ba2
alexis <alexisrodriguezossa99@gmail.com>
2021-08-31 21:54:36 -0300

    test1

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 
-const path = express();
+const app = express();
 
 const path = require('path');
 
-app.use(express.static(dirname + '/angularapp'));
+app.use(express.static(__dirname + '/angularapp'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(dirname + '/angularapp/index.html'));
+    res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
 
 console.log('Build Successful');
